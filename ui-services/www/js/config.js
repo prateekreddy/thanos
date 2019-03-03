3b8cc101033ff3a605719213e0bf61561b611348
Prateek Reddy <prateekreddyy@gmail.com>
2019-03-03 20:34:05 +0530

    update config.js

--- ui-services/www/js/config.js
+++ ui-services/www/js/config.js
@@ -1,4 +1,4 @@
 var thanosConfig = {
-    "loginServer":"http://34.220.50.194",
-    "mongoService":"http://34.220.50.194"
+    "loginServer":"http://192.168.0.106",
+    "mongoService":"http://192.168.0.106"
 }
